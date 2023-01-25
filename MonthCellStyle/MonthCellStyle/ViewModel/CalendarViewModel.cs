baaaad6d54310d18186841885f313f6f1c3f95e3
VishalOmprasad <vishal.omprasad@syncfusion.com>
2023-01-25 11:05:36 +0530

    New KB sample added

---
+++ MonthCellStyle/MonthCellStyle/ViewModel/CalendarViewModel.cs
@@ -0,0 +1,12 @@
+﻿namespace MonthCellStyle
+{
+    public class CalendarViewModel
+    {
+        public List<DayOfWeek> WeekEndDayList { get; set; }
+
+        public CalendarViewModel()
+        {
+            this.WeekEndDayList = new List<DayOfWeek>() { DayOfWeek.Sunday, DayOfWeek.Saturday };
+        }
+    }
+}
